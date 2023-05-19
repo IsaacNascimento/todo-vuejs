74e902315cc1d693b65f791717845739d5387c56
Isaac Nascimento <isaacs.nascimento17@gmail.com>
2023-05-19 16:13:57 -0300

    Code improved

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container grid-xs py-2">
+      <img
+        class="img-responsive img-logo"
+        src="https://via.placeholder.com/150"
+        alt="LogoMarca"
+      />
+      <form ref="addTarefa" @submit.prevent="addTodo">
+        <div class="input-group">
+          <input
+            type="text"
+            v-model="tarefa.description"
+            class="form-input"
+            placeholder="Nova tarefa"
+          />
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+      </form>
+      <div class="todo-list">
+        <tileTarefa v-for="item in tarefas" :key="item.id" :tarefa="item" @toggle="toggleTarefa" @remove="removerTarefa"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TileTarefa from './components/TileTarefa.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { TileTarefa },
+  data() {
+    return {
+      tarefas: [],
+      tarefa: {
+        checked: false,
+      },
+    };
+  },
+  methods: {
+    addTodo() {
+      const id = Date.now();
+      const { checked, description } = this.tarefa;
+      this.tarefas.push({ checked, id, description });
+      console.log(this.tarefas);
+      this.tarefa.description = "";
+    },
+
+    toggleTarefa(tarefa) {
+      const index = this.tarefas.findIndex(item => item.id === tarefa.id);
+      if (index >= 0) {
+        const checked = !this.tarefas[index].checked;
+        this.tarefas[index].checked = checked;
+      }
+    },
+    removerTarefa(tarefa) {
+      const index = this.tarefas.findIndex(item => item.id === tarefa.id);
+      if (index >= 0) {
+        this.tarefas.splice(index, 1);
+      }
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.img-logo {
+  max-width: 200px;
+  margin: 0 auto;
+  margin-bottom: 10px;
+}
+
+.todo-list {
+  padding-top: 2rem;
 }
 </style>
